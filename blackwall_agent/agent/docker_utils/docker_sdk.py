b5752d6c2cc4fdf3f3745815a042d7de102bd280
Mirialis <arcadia16@atomicmail.io>
2025-05-18 21:14:56 +0300

    Change docker image approach, fix CVE-2025-4802

--- blackwall_agent/agent/docker_utils/docker_sdk.py
+++ blackwall_agent/agent/docker_utils/docker_sdk.py
@@ -1,24 +1,9 @@
 import docker
-from docker import errors
+from .dockerfile_repo import image_map
 
 client = docker.from_env()
 base_image = "ubuntu:latest"
 
-def build_image(dockerfile_path: str):
-    try:
-        result = client.images.build(path=dockerfile_path)
-        return result
-    except errors.BuildError:
-        print(f"{__name__} :: Build error for {dockerfile_path}")
-        return None
-    except errors.APIError:
-        print(f"{__name__} :: Unknown Docker error")
-        return None
-    except TypeError:
-        print(f"{__name__} :: Neither path nor fileobj is specified")
-        return None
 
 def test_docker():
     print(client.containers.run("ubuntu:latest", "echo Hello World"))
-
-test_docker()
---
+++ blackwall_agent/agent/docker_utils/dockerfile_repo.py
@@ -0,0 +1,6 @@
+image_map = {
+    "ssh": "blackwall-ssh:latest",
+    "ftp": "blackwall-ftp:latest",
+    "mysql": "blackwall-mysql:latest",
+    "telnet": "blackwall-telnet:latest"
+}
